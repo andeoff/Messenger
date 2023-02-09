3ed7fe5548e2a544feaf884914e979f682f2dc0e
Andrey Ivantsov <mcandrey45@gmail.com>
2023-02-09 21:09:23 +0300

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Andrey on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
